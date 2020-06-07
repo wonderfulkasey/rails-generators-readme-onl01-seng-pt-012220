9829a862d8cbeab1e06acf3a35df30632672e62d
Kasey Arnold <kaseyarnold15@gmail.com>
2020-06-07 02:17:39 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'categories/show'
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
-  resources :categories
-end
+  resources :accounts
+    get 'accounts/new'
+  
+    get 'accounts/create'
+  
+    get 'accounts/edit'
+  
+    get 'accounts/update'
+  
+    get 'accounts/destroy'
+  
+    get 'accounts/index'
+  
+    get 'accounts/show'
+  
+    get 'admin/dashboard'
+  
+    get 'admin/stats'
+  
+    get 'admin/financials'
+  
+    get 'admin/settings'
+  
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get 'categories/show'
+  
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+    resources :categories
+  end
